82115735e56d1fc322f54f50902eaa48987c7fce
Sara Juršetić <sara.jursetic@gmail.com>
2025-06-03 09:11:14 +0200

    Add files via upload

---
+++ vjezba10.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP ponavljanje</title>
+    <style>
+        *{
+        }
+        body{
+            margin-top:15%;
+            margin-left:40%;
+            background-color: beige;
+        }
+    </style>
+</head>
+<body>
+    
+<h1>PHP ponavljanje</h1>
+
+<h2>Prijava</h2>
+
+<form action="login.php" method="POST">
+
+        <input type="text" name="username"> Korisničko ime:
+        <br><br>
+
+        <input type="password" name="password"> Lozinka:
+        <br><br>
+
+        <button type="submit">Prijava</button>
+
+</form>
+
+
+</body>
+</html>
